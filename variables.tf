b76afcc5fd7f56c9ebce787f7d2ff7325b7d7fae
Edgar Hernandez <edherna@microsoft.com>
2025-04-03 04:13:46 -0300

    Initial code for RSG

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "location" {
+  type        = string
+  description = "Required. The Azure region for deployment of the this resource."
+  nullable    = false
+}
+
+variable "application_code" {
+  type        = string
+  description = "Required. Application code or service code."
+  nullable    = false
+}
+
+variable "environment" {
+  type        = string
+  description = "Required. Application environment (P, C, D, F, E)."
+  nullable    = false
+}
+
+variable "lock" {
+  type = object({
+    kind = string
+    name = optional(string, null)
+  })
+  default     = null
+  description = <<DESCRIPTION
+  Controls the Resource Lock configuration for this resource. The following properties can be specified:
+  
+  - `kind` - (Required) The type of lock. Possible values are `\"CanNotDelete\"` and `\"ReadOnly\"`.
+  - `name` - (Optional) The name of the lock. If not specified, a name will be generated based on the `kind` value. Changing this forces the creation of a new resource.
+  DESCRIPTION
+
+  validation {
+    condition     = var.lock != null ? contains(["CanNotDelete", "ReadOnly"], var.lock.kind) : true
+    error_message = "Lock kind must be either `\"CanNotDelete\"` or `\"ReadOnly\"`."
+  }
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = null
+  description = "(Optional) Tags of the resource."
+}
